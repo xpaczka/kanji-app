8bc8f8049069b0dd790ec9a745915077dc0734c4
Michał Paczyński <michal.paka@op.pl>
2025-03-02 16:26:49 +0100

    Add back button to main page

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,9 +1,16 @@
+import { Button } from '#/components/ui/button'
 import { SignIn as SignInForm } from '@clerk/nextjs'
+import Link from 'next/link'
 
 export default function SignIn() {
   return (
-    <div className='min-h-screen grid items-center justify-items-center'>
-      <SignInForm />
-    </div>
+    <>
+      <Button className='absolute'>
+        <Link href='/'>Back to main page</Link>
+      </Button>
+      <div className='min-h-screen grid items-center justify-items-center'>
+        <SignInForm />
+      </div>
+    </>
   )
 }
