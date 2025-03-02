f72c61ebeb098ff3dfcd4137ce75b5a52453de16
Michał Paczyński <michal.paka@op.pl>
2025-03-02 18:31:11 +0100

    Add shared layout for dashboard pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 
 export default function Home() {
   return (
-    <div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
+    <div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
       <main className='flex gap-8 row-start-2 items-center sm:items-start'>
         <SignedOut>
           <Button>
