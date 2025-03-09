53d41665e5add71db70612310fad7786dbb51047
Michał Paczyński <michal.paka@op.pl>
2025-03-09 11:27:40 +0100

    Detect domain URL for API endpoints

--- src/app/_trpc/TrpcProvider.tsx
+++ src/app/_trpc/TrpcProvider.tsx
@@ -3,18 +3,13 @@
 import { ReactNode, useState } from 'react'
 import { trpc } from './client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { httpBatchLink } from '@trpc/client'
+import { TRPC_LINKS } from '#/constants/misc'
 
 export default function TrpcProvider({
   children,
 }: Readonly<{ children: ReactNode }>) {
   const [queryClient] = useState(() => new QueryClient({}))
-
-  const [trpcClient] = useState(() =>
-    trpc.createClient({
-      links: [httpBatchLink({ url: 'http://localhost:3000/api/trpc' })],
-    })
-  )
+  const [trpcClient] = useState(() => trpc.createClient(TRPC_LINKS))
 
   return (
     <trpc.Provider client={trpcClient} queryClient={queryClient}>
--- src/app/_trpc/server-client.ts
+++ src/app/_trpc/server-client.ts
@@ -1,10 +1,6 @@
-import { httpBatchLink } from '@trpc/client'
 import { appRouter } from '#/server'
+import { TRPC_LINKS } from '#/constants/misc'
 
 export const serverClient = appRouter.createCaller({
-  links: [
-    httpBatchLink({
-      url: 'http://localhost:3000/api/trpc',
-    }),
-  ],
+  links: TRPC_LINKS,
 })
