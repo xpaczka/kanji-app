b9b4a82f436093814d484977a1f43bd413496e3c
Michał Paczyński <michal.paka@op.pl>
2025-03-12 17:03:33 +0100

    Implement `SessionLayout` for all session pages

---
+++ src/app/(sessions)/layout.tsx
@@ -0,0 +1,16 @@
+import { ReactNode } from 'react'
+import { Button } from '#/components/ui/button'
+
+export default function SessionLayout({ children }: { children: ReactNode }) {
+  return (
+    <div className='relative w-full h-screen flex flex-col justify-center items-center'>
+      <div className='absolute top-0 left-0 right-0 px-10 pt-6 flex justify-between items-center'>
+        {/* TODO: Dynamically generate page title */}
+        <h1 className='text-lg'>Flashcards</h1>
+        {/* TODO: Implement end session handling */}
+        <Button>End session</Button>
+      </div>
+      {children}
+    </div>
+  )
+}
