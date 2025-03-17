3d89785089a8a65ac33000e6673adfef22030112
Michał Paczyński <michal.paka@op.pl>
2025-03-17 12:47:19 +0100

    Read page header from `sessionType` property

--- src/app/(sessions)/layout.tsx
+++ src/app/(sessions)/layout.tsx
@@ -1,12 +1,12 @@
 import { ReactNode } from 'react'
 import EndAppSessionButton from '#/components/buttons/EndAppSessionButton'
+import SessionPageHeader from '#/components/layout/SessionPageHeader'
 
 export default function SessionLayout({ children }: { children: ReactNode }) {
   return (
     <div className='relative w-full h-screen flex flex-col justify-center items-center'>
       <div className='absolute top-0 left-0 right-0 px-10 pt-6 flex justify-between items-center'>
-        {/* TODO: Dynamically generate page title */}
-        <h1 className='text-lg'>Flashcards</h1>
+        <SessionPageHeader />
         <EndAppSessionButton />
       </div>
       {children}
