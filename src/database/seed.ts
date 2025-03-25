39487aa0a6b5d9c163188922dacfbcaa913c3681
Michał Paczyński <michal.paka@op.pl>
2025-03-25 20:49:42 +0100

    Use `drizzle-seed` to seed local database

--- src/database/seed.ts
+++ src/database/seed.ts
@@ -3,6 +3,7 @@ import { data as kanjiData } from '../../scripts/data.json'
 import { KanjiItemJlptLevel, kanjiTable } from './schema'
 import { v4 as uuid } from 'uuid'
 import { drizzle } from 'drizzle-orm/node-postgres'
+import { reset, seed } from 'drizzle-seed'
 
 const args = process.argv.slice(2)
 const argsMap = new Map()
@@ -25,7 +26,7 @@ const main = async () => {
   const pool = new Pool({ connectionString: databaseUrl })
   const database = drizzle(pool)
 
-  const data: (typeof kanjiTable.$inferInsert)[] = kanjiData.map((item) => ({
+  const kanji: (typeof kanjiTable.$inferInsert)[] = kanjiData.map((item) => ({
     id: uuid(),
     kanji: item.kanji,
     level: item.level as KanjiItemJlptLevel,
@@ -34,9 +35,13 @@ const main = async () => {
     kun_readings: item.kun_readings ?? [],
   }))
 
+  console.log('Resetting database...')
+
+  await reset(database, { kanji })
+
   console.log('Seeding database..')
 
-  await database.insert(kanjiTable).values(data)
+  await seed(database, { kanji })
 
   console.log('Seeding completed')
 }
