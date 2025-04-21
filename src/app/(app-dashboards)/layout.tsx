87ad7caa9eb23310742adf7228b168db76515fca
Michał Paczyński <michal.paka@op.pl>
2025-04-21 18:44:42 +0200

    Remove Zustand user store
    Now it is passed as context in TRPC router

--- src/app/(app-dashboards)/layout.tsx
+++ src/app/(app-dashboards)/layout.tsx
@@ -1,19 +1,15 @@
 import BreadcrumbsNavigation from "#/components/navigation/BreadcrumbsNavigation"
 import TopNavigation from "#/components/navigation/TopNavigation"
 import { ReactNode } from "react"
-import { createServerClient } from "../_trpc/server-client"
 
 export default async function AppRoutesLayout({
   children
 }: {
   children: ReactNode
 }) {
-  const serverClient = await createServerClient()
-  const user = await serverClient.user.getUser()
-
   return (
     <>
-      <TopNavigation user={user} />
+      <TopNavigation />
       <div className="p-10">
         <BreadcrumbsNavigation />
         {children}
