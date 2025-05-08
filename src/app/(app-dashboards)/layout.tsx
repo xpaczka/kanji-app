42988ce561bccaee6a6bfdb1f2abaa712cb848da
Michał Paczyński <michal.paka@op.pl>
2025-05-08 20:06:09 +0200

    Remove redirect from dashboard page

--- src/app/(app-dashboards)/layout.tsx
+++ src/app/(app-dashboards)/layout.tsx
@@ -2,28 +2,13 @@
 
 import BreadcrumbsNavigation from "#/components/navigation/BreadcrumbsNavigation"
 import TopNavigation from "#/components/navigation/TopNavigation"
-import { ReactNode, useEffect } from "react"
-import { trpc } from "../_trpc/client"
-import { useNavigation } from "#/hooks"
-import { ROUTES } from "#/constants/router"
+import { ReactNode } from "react"
 
-// TODO: Add loading state
 export default function KnowledgeTestLayout({
   children
 }: {
   children: ReactNode
 }) {
-  const { navigate } = useNavigation()
-
-  const { data: knowledgeEvaluationLevel, isFetched } =
-    trpc.user.getUserKnowledgeEvaluationLevel.useQuery()
-
-  useEffect(() => {
-    if (isFetched && !knowledgeEvaluationLevel) {
-      navigate(ROUTES.knowledgeTest)
-    }
-  }, [knowledgeEvaluationLevel, isFetched, navigate])
-
   return (
     <>
       <TopNavigation />
